5097f78557c587c9151fe204bb7154492acc3c62
MilosOst <ostojicmilos10@gmail.com>
2023-04-29 11:35:44 -0400

    Initial Commit

---
+++ recipe-app/recipe_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipe_appApp.swift
+//  recipe-app
+//
+//  Created by Milos on 2023-04-29.
+//
+
+import SwiftUI
+
+@main
+struct recipe_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
